weiyilai/grpc

commit 14714b3512ee5e720186fb8f0d8ce9322834c014

Fix issues/36613 (#37022)
Creating a new PR in continuation to #36894 to fix post-submit errors. I've re-added the `from ._metadata import Metadata`  statement to fix the post-submit error.

Closes #37022

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/37022 from sreenithi:fix-issues/36613 beb5b5c63d9206eb715b9999ed89de9fbf776582
PiperOrigin-RevId: 646142324

src/python/grpcio/grpc/aio/_base_server.py
@@ -18,6 +18,7 @@ from typing import Generic, Iterable, Mapping, NoReturn, Optional, Sequence
 
 import grpc
 
+from ._metadata import Metadata  # pylint: disable=unused-import
 from ._typing import DoneCallbackType
 from ._typing import MetadataType
 from ._typing import RequestType

commit 1ad4b41aab4141e92b98e1be35383ceb3039086b

[Python] Remove unused import of `Metadata` from `aio._base_server.py`. (#42962)
Remove unused import of `Metadata` from `_base_server.py`. Removing this import had caused some internal test breakage about 2+ years ago and hence had to be re-introduced in https://github.com/grpc/grpc/pull/37022

Trying to remove it again now to see if this unused import is still needed or safe to remove. Internal tests doesn't show any failures associated with this change now, so I assume this is fine.

Closes #42962

COPYBARA_INTEGRATE_REVIEW=https://github.com/grpc/grpc/pull/42962 from sreenithi:remove_unused_import 876e4adce59c9cf058bf92f0bfb7556ec33f4942
PiperOrigin-RevId: 947078630

src/python/grpcio/grpc/aio/_base_server.py
@@ -18,7 +18,6 @@ from typing import Generic, Iterable, Mapping, NoReturn, Optional, Sequence
 
 import grpc
 
-from ._metadata import Metadata  # pylint: disable=unused-import
 from ._typing import DoneCallbackType
 from ._typing import MetadataType
 from ._typing import RequestType